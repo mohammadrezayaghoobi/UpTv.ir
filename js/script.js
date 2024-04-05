12db12a0e908fe6350f45c70dbfa92fb14ba2767
mohammadrezayaghoobi <99791931+mohammadrezayaghoobi@users.noreply.github.com>
2024-04-05 14:19:38 +0330

    Update script.js
    add js for messagebox

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+let  btnShow = document.getElementById('submitForm');
+let msgBox = document.querySelector('.message');
+let closeBtn = document.querySelector('.message__x');
+
+let msgBox1 = document.querySelector('.message1');
+let closeBtn1 = document.querySelector('.message__x1');
+
 let navbar = document.querySelector(".menu-bars");
 document.querySelector("#menu-btn").onclick = () => {
   navbar.classList.toggle("active");
@@ -5,3 +12,32 @@ document.querySelector("#menu-btn").onclick = () => {
 document.querySelector("#x").onclick = () => {
     navbar.classList.remove("active");
   };
+
+
+
+btnShow.onclick = function() {
+  let serverStatus = (Math.random()*1).toFixed();
+  console.log(serverStatus);
+
+  if (serverStatus == 1) {
+    msgBox.classList.add('show');
+
+    closeBtn.onclick = function () {
+      msgBox.classList.remove('show');
+    }
+    
+    setTimeout(() => {
+      msgBox.classList.remove('show')
+    }, 3500);
+  } else {
+    msgBox1.classList.add('show');
+  
+    closeBtn1.onclick = function () {
+      msgBox1.classList.remove('show');
+    }
+  
+    setTimeout(() => {
+      msgBox1.classList.remove('show')
+    }, 3500);
+  }
+}
